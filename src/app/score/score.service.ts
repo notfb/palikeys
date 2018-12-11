83b1cb7d1531a1bd91eabfd37314200c1fdf0245
notfb <fabian.bieker@web.de>
2018-12-11 16:04:09 +0100

    fix typo

--- src/app/score/score.service.ts
+++ src/app/score/score.service.ts
@@ -9,16 +9,16 @@ import {Score} from './_models/scroe.model';
 })
 export class ScoreService {
 
-  readonly baseUrl: 'https://palikeys-serverless.firebaseapp.com/api/score';
+  private static readonly BASE_URL = 'https://palikeys-serverless.firebaseapp.com/api/score';
 
   constructor(private http: HttpClient) {
   }
 
   update(id: string, score: number): Observable<Object> {
-    return this.http.patch(`${this.baseUrl}/${id}`, {score});
+    return this.http.patch(`${ScoreService.BASE_URL}/${id}`, {score});
   }
 
   get(id: string): Observable<Score> {
-    return this.http.get(`${this.baseUrl}/${id}`) as Observable<Score>;
+    return this.http.get(`${ScoreService.BASE_URL}/${id}`) as Observable<Score>;
   }
 }

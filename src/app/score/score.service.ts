304d762ad2ca538da3375316e7360425d89ad6b9
notfb <fabian.bieker@web.de>
2018-12-11 20:49:39 +0100

    TODOs

--- src/app/score/score.service.ts
+++ src/app/score/score.service.ts
@@ -3,6 +3,10 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Score} from './_models/scroe.model';
 
+// TODO: instead of using update to set a new score, send the score from current lesson as an increment and ...
+// TODO: make updates / increments work offline (aka store and send later...)
+// https://blog.formpl.us/how-to-handle-post-put-requests-in-offline-applications-using-service-workers-indexedb-and-da7d0798a9ab
+
 // TODO: introduce generic REST service for mocking in tests etc.
 @Injectable({
   providedIn: 'root'
@@ -15,8 +19,6 @@ export class ScoreService {
   }
 
   update(id: string, score: number): Observable<Object> {
-    // TODO: make this work when offline (aka store and send later...)
-    // https://blog.formpl.us/how-to-handle-post-put-requests-in-offline-applications-using-service-workers-indexedb-and-da7d0798a9ab
     return this.http.patch(`${ScoreService.BASE_URL}/${id}`, {score});
   }
 

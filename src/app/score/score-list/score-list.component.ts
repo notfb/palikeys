733236da8dc2c5e4516edb66d26d6bbb071762e2
notfb <fabian.bieker@web.de>
2018-12-11 19:42:28 +0100

    add missing errorMessage

--- src/app/score/score-list/score-list.component.ts
+++ src/app/score/score-list/score-list.component.ts
@@ -10,12 +10,13 @@ import {Score} from '../_models/scroe.model';
 export class ScoreListComponent implements OnInit {
   scores: Score[] = [{username: 'test', score: 23, id: 'testId'}];
   displayedColumns: string[] = ['name', 'score'];
+  errorMessage: string | undefined;
 
   constructor(private scoreService: ScoreService) {
   }
 
   ngOnInit() {
-    this.scoreService.list().subscribe(list => this.scores = list);
+    this.scoreService.list().subscribe(list => this.scores = list, error => this.errorMessage = error.message || error);
   }
 
 }
